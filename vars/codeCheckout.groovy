0eee191d79e5b2bd2f93baacdc79f2b1744dd3ba
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-21 18:12:20 +0800

    test

--- vars/codeCheckout.groovy
+++ vars/codeCheckout.groovy
@@ -1,6 +1,19 @@
 #!/usr/bin/env groovy
-@Library("shareMaven") _
-def call(string repo='') {
+/*
+svnRepo 是必须指定的属性值。
+svnCredentialsId 证书id，为可选参数，不填为默认值
+svnLocal 代码checkout地址，为可选参数，不填为默认值
+*/
+def call(body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  config.svnCredentialsId = null==config.svnCredentialsId ? "c9baf728-2463-4d59-8643-2181a681fdd4" : config.svnCredentialsId
+  config.svnLocal = null==config.svnLocal ? "." : config.svnLocal
+  def svnRepo="${config.svnRepo}"
+
   checkout([$class: 'SubversionSCM',
   additionalCredentials: [],
   excludedCommitMessages: '',
@@ -10,6 +23,6 @@ def call(string repo='') {
   filterChangelog: false,
   ignoreDirPropChanges: false,
   includedRegions: '',
-  locations: [[credentialsId: 'c9baf728-2463-4d59-8643-2181a681fdd4', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "${repo}"]],
+  locations: [[credentialsId: "${config.svnCredentialsId}", depthOption: 'infinity', ignoreExternalsOption: true, local: "${svnLocal}", remote: "${svnRepo}"]],
   workspaceUpdater: [$class: 'UpdateUpdater']])
 }
