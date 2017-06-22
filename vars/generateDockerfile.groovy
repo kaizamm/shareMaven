21c4e1ae3e0b9b1009b94401e6585cc50a943e4b
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-22 16:57:44 +0800

    test

--- vars/generateDockerfile.groovy
+++ vars/generateDockerfile.groovy
@@ -41,7 +41,7 @@ ADD ${env.appTargetName}.war \${CATALINA_HOME}/webapps
 RUN cd \${CATALINA_HOME}/webapps && unzip ${env.appTargetName}.war -d ${env.appTargetName} && rm -rf ${env.appTargetName}.war
     """
 
-    def svnRevision = sh (script: "svn info ${env.WORKSPACE}/${env.appTargetName} |grep 'Last Changed Rev' | awk '{print \$4}'",returnStdout: true).trim()
+    def env.svnRevision = sh (script: "svn info ${env.WORKSPACE}/${env.appTargetName} |grep 'Last Changed Rev' | awk '{print \$4}'",returnStdout: true).trim()
     // 生成Dockerfile
     sh (script: "rm -rf ${buildPath}",returnStdout: true)
     sh (script: "mkdir -p ${buildPath}",returnStdout: true)
