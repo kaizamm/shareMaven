3a34389bbfe84228b78c8e8bf95d7c537aadb6dc
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-22 18:09:34 +0800

    test

--- vars/deployContainer.groovy
+++ vars/deployContainer.groovy
@@ -34,12 +34,12 @@ def call(body) {
     def dockerRunOpt="${env.dockerRunOpt}"
     def dockerHosts="${env.dockerHosts}"
 println dockerHosts
-    def hostsArry = dockerHosts.split('\n')
-    for (int i = 0;i<hostsArry.size();i++) {
-      def appAddress = hostsArry[i].split(',')[0].trim()
+    // def hostsArry = dockerHosts.split(' ')
+    for (int i = 0;i<dockerHosts.size();i++) {
+      def appAddress = dockerHosts[i].split(',')[0].trim()
       def appIp = appAddress.split('_')[0].trim()
       def appPort = appAddress.split('_')[1].trim()
-      def appExpose = hostsArry[i].split(',')[1].trim()
+      def appExpose = dockerHosts[i].split(',')[1].trim()
       def instanceId = (appOrg+"_"+appEnv+"_"+appTargetName).toUpperCase().trim()
       def containerName = (instanceId+"_"+appIp+"_"+appPort).toUpperCase().replace(".","").trim()
       def int jmxPort = (appPort.toInteger()+10)
