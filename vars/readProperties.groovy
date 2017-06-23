9c0a1763a17cf9280729674978c50fcece367eb5
王少泽 <shaozewang@quarkfinance.com>
2017-06-23 09:22:10 +0000

    Update readProperties.groovy

--- vars/readProperties.groovy
+++ vars/readProperties.groovy
@@ -1,11 +1,9 @@
 #!/usr/bin/env groovy
 def call(path){
-  def envList = []
-  def props = readProperties file: path
-  for (it2 in mapToList(props)) {
-      def key=it2[0]
-      def val=it2[1]
-      envList << key+"="+val
-  }
-  return envList
+	def props = readProperties file: path
+	def dlist = []
+	for (entry in props) {
+		dlist.add(entry.key+"="+entry.value)
+	}
+	return dlist
 }
