ec5e75f56489006af4adfc988a68760cd9905d41
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-26 16:16:22 +0800

    test

--- vars/choiceAction.groovy
+++ vars/choiceAction.groovy
@@ -2,5 +2,5 @@ def call() {
   def actionInput = input (
     id: 'actionInput', message: 'Choice your action!', parameters: [[$class: 'ChoiceParameterDefinition', choices: "deploy\nrollback", description: 'choice your action!', name: 'action']]
     )
-  return action = actionInput.trim()
+  return actionInput.trim()
 }
