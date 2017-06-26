58eb39fba039cbf679f1129c532341f66231b2a2
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-26 17:21:20 +0800

    test

--- vars/rollbackContainer.groovy
+++ vars/rollbackContainer.groovy
@@ -33,8 +33,9 @@ def call(body) {
       def toImage="${env.DOCKER_REGISTRY}"+"/"+"${rollbackAppTargetName}"
       def appCfgs="${env.appCfgs}"
       def projectRecipintList="${env.projectRecipintList}"
-      def dockerRunOpt="${env.dockerRunOpt}"
+      def dockerRunOpts="${env.dockerRunOpts}"
       def dockerHosts="${env.dockerHosts}"
+      def javaOpts="${env.javaOpts}"
 
     stage('确认回滚到生产') {
       input "Your choice is ${rollbackAppTargetName} \nAre you sure deploy to Production?"
@@ -72,7 +73,8 @@ def call(body) {
             // 拉取push到registry的image
             sh (script: "docker -H"+" "+appIp+":2375 pull"+" "+toImage,returnStdout: true)
             // 运行容器
-            sh (script: "docker -H"+" "+appIp+":2375 run -d --restart=always --name="+containerName+" "+"-e etcdClusterIp="+etcdClusterIp+" "+"-e appCfgs="+appCfgs.replace("null","").trim()+" "+"-e appTargetName="+appTargetName+" "+"-e instanceId="+instanceId+" "+"-e jmxIp="+appIp+" "+"-e jmxPort="+jmxPort+" "+"-e JAVA_OPTS='-server -Xms12g -Xmx12g -Xss512k -XX:PermSize=512m -XX:MaxPermSize=768m -XX:+AggressiveOpts -XX:+UseBiasedLocking -XX:MaxTenuringThreshold=7 -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:+UseFastAccessorMethods -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -XX:+UseParNewGC' -v /data/logs/"+containerName+":/AppLogs -p"+" "+jmxPort+":"+jmxPort+" "+"-p"+" "+appExpose+" "+dockerRunOpt.replace("null","").trim()+" "+toImage.trim(),returnStdout: true)
+            // sh (script: "docker -H"+" "+appIp+":2375 run -d --restart=always --name="+containerName+" "+"-e etcdClusterIp="+etcdClusterIp+" "+"-e appCfgs="+appCfgs.replace("null","").trim()+" "+"-e appTargetName="+appTargetName+" "+"-e instanceId="+instanceId+" "+"-e jmxIp="+appIp+" "+"-e jmxPort="+jmxPort+" "+"-e JAVA_OPTS='-server -Xms12g -Xmx12g -Xss512k -XX:PermSize=512m -XX:MaxPermSize=768m -XX:+AggressiveOpts -XX:+UseBiasedLocking -XX:MaxTenuringThreshold=7 -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:+UseFastAccessorMethods -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -XX:+UseParNewGC' -v /data/logs/"+containerName+":/AppLogs -p"+" "+jmxPort+":"+jmxPort+" "+"-p"+" "+appExpose+" "+dockerRunOpts.replace("null","").trim()+" "+toImage.trim(),returnStdout: true)
+            sh (script: "docker -H"+" "+appIp+":2375 run -d --restart=always --name="+containerName+" "+"-e etcdClusterIp="+etcdClusterIp+" "+"-e appCfgs="+appCfgs.replace("null","").trim()+" "+"-e appTargetName="+appTargetName+" "+"-e instanceId="+instanceId+" "+"-e jmxIp="+appIp+" "+"-e jmxPort="+jmxPort+" "+"-e JAVA_OPTS="+javaOpts.trim()+" " +"-v /data/logs/"+containerName+":/AppLogs -p"+" "+jmxPort+":"+jmxPort+" "+"-p"+" "+appExpose+" "+dockerRunOpts.replace("null","").trim()+" "+toImage.trim(),returnStdout: true)
 
             sleep(10)
             // 获取当前运行容器的状态码
