7756ef39cf4a4e6d9a1c9a6aec12ef5d94912659
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-26 15:25:06 +0800

    test

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,21 +1,7 @@
 #!/usr/bin/env groovy
 /*
-def call(String buildStatus = 'STARTED',String emailTo = '') {
-	buildStatus =  buildStatus ?: 'SUCCESSFUL'
-	def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-	def summary = "${subject} (${env.BUILD_URL})"
-	def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-	<p>点击查看详情 &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
-	<p>此邮件为<b>开发环境</b>jenkins发出。</p>
-	<p>如有疑问请联系 ChenglanGuo@quarkfinance.com</p>"""
-	emailext attachLog: true,
-	body: details, mimeType: 'text/html', replyTo: "${env.projectRecipientList}",
-	subject: subject,
-	recipientProviders:  [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'CulpritsRecipientProvider'], [$class: 'FailingTestSuspectsRecipientProvider'], [$class: 'FirstFailingBuildSuspectsRecipientProvider'], [$class: 'UpstreamComitterRecipientProvider']],
-	to: emailTo
-}
+需要指定emailRecipients,来源于jenkinspipeline.properties中的projectRecipientList，以逗号分隔
 */
-
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
@@ -35,5 +21,6 @@ def call(body) {
         emailext(body: content, mimeType: 'text/html',
                 replyTo: '$DEFAULT_REPLYTO', subject: subject,
                 to: config.emailRecipients, attachLog: attachLog, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider']])
+        error "exit"
     }
 }
