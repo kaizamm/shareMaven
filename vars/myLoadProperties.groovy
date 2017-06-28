fdbc79dea94922818ab8814a19a8f91a58e81197
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-28 20:10:51 +0800

    add returnStdout in context

--- vars/myLoadProperties.groovy
+++ vars/myLoadProperties.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
 // def call(path){
 def call(String host,int port=2379,String location){
-	def context = sh "/data/jenkins_etcd/etcdRead.py ${host} ${port} ${location}"
-	println context
+	def context = sh (script: "/data/jenkins_etcd/etcdRead.py ${host} ${port} ${location}",returnStdout: true)
 	// def props = readProperties file: path
 	def props = readProperties text: context
 	def dlist = []
