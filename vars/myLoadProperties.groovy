9484a7a092a9e1bc507a90dd4c5054784413e783
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-28 20:02:13 +0800

    change etcdRead.py location

--- vars/myLoadProperties.groovy
+++ vars/myLoadProperties.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 // def call(path){
 def call(String host,int port=2379,String location){
-	def context = sh "/data/etcdRead.py ${host} ${port} ${location}"
+	def context = sh "/data/jenkins_etcd/etcdRead.py ${host} ${port} ${location}"
 	// def props = readProperties file: path
 	def props = readProperties text: context
 	def dlist = []
