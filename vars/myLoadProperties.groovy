edb7ed35e310c230e17a2acf30e00a14e31d45cf
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-28 19:59:25 +0800

    add readProperties from etcd

--- vars/myLoadProperties.groovy
+++ vars/myLoadProperties.groovy
@@ -1,6 +1,9 @@
 #!/usr/bin/env groovy
-def call(path){
-	def props = readProperties file: path
+// def call(path){
+def call(String host,int port=2379,String location){
+	def context = sh "/data/etcdRead.py ${host} ${port} ${location}"
+	// def props = readProperties file: path
+	def props = readProperties text: context
 	def dlist = []
 	for (entry in props) {
 		dlist.add(entry.key+"="+entry.value)
