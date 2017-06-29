60d4624d139583fc542521bb8b1b386cac96cd63
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-29 11:40:24 +0800

    test

--- vars/myLoadProperties.groovy
+++ vars/myLoadProperties.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 // def call(path){
-def call(String host,int port,String location,env){
+def call(String host,int port,String location){
 	def context = sh (script: "/data/jenkins_etcd/etcdRead.py ${host} ${port} ${location}",returnStdout: true)
 	// def props = readProperties file: path
 	def props = readProperties text: context
