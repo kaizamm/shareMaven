7ba210c2e7967d05bb0ae841117131935cbd75e0
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-07-11 11:28:24 +0800

    add gitCodeCheckout

---
+++ vars/gitCodeCheckout.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/env groovy
+/*
+gitRepo
+gitCredentialsId
+gitLocal
+*/
+
+def call(body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  config.gitCredentialsId = null == config.gitCredentialsId ? "c9baf728-2463-4d59-8643-2181a681fdd4" : config.gitCredentialsId
+  config.gitLocal = null == config.gitLocal ? "." : config.gitLocal
+  def gitRepo = config.gitRepo
+
+  dir("${config.gitRepo}") {
+    checkout([$class: 'GitSCM',
+    branches: [[name: '*/master']],
+    userRemoteConfigs: [[url: "${gitRepo}",credentialsId: "${config.credentialsId}"]]])
+  }
+}
