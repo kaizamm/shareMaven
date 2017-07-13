0c5c0093171ca52c297647aeb3b9fb6e37adaac6
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-07-13 20:41:26 +0800

    test

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -32,7 +32,7 @@ RUN cd ${env.remoteDir} && unzip ${packageName} -d ${packageUnzipName}
   // 生成env上下文的imageTag
   for (x in dirList) {
     if (x == '.git'){
-      env.svnRevision = sh (script: "cd ${projectPath} && git rev-parse HEAD |awk '{print \$1}'",returnStdout: true).trim()
+      env.svnRevision = sh (script: "cd ${projectPath} && git rev-parse HEAD |awk '{print \$1}'",returnStdout: true).trim().substring(0,7)
       // env.svnRevision = 'test'
     } else {
       env.svnRevision = sh (script: "svn info ${projectPath} |grep 'Last Changed Rev' | awk '{print \$4}'",returnStdout: true).trim()
