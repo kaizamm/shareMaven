53a481937fad61a6e532370d4c747aaf383df4b6
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-09-14 18:49:29 +0800

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -25,7 +25,7 @@ def call(body) {
   //Dockerfile内容
   def dockerFileContext="""FROM ${env.fromImage}
 MAINTAINER devops "devops@quarkfinance.com"
-ADD ${buildPath}/*.war ${buildPath}/*.jar ${env.remoteDir}/
+ADD *.war *.jar ${env.remoteDir}/
 RUN cd ${env.remoteDir} && unzip ${packageName} -d ${packageUnzipName}
     """
 
