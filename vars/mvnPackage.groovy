f9da6d3e6e650c9a70faa4e2195d03262ac23600
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-09-15 11:48:40 +0800

    restore

--- vars/mvnPackage.groovy
+++ vars/mvnPackage.groovy
@@ -1,10 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(args,dir) {
+def call(args) {
   args = null==args ? "clean install -Dmaven.test.skip=true" : args
-  dir = null==dir ? "" : dir
-  if (${dir}) {
-    sh "cd ${env.WORKSPACE}/${dir} && touch text.txt"
-  }
   sh "${tool 'M3'}/bin/mvn ${args}"
 }
