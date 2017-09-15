97202807de39761b13659e8fca817fe6c2b3211a
Chongqiang Zhong <chongqiangzhong@quarkfinance.com>
2017-09-15 03:19:25 +0000

    Update mvnPackage.groovy

--- vars/mvnPackage.groovy
+++ vars/mvnPackage.groovy
@@ -4,7 +4,7 @@ def call(args,dir) {
   args = null==args ? "clean install -Dmaven.test.skip=true" : args
   dir = null==dir ? "" : dir
   if (${dir}) {
-    sh "cd ${WORKSPACE}/${dir} && touch text.txt"
+    sh "cd ${env.WORKSPACE}/${dir} && touch text.txt"
   }
   sh "${tool 'M3'}/bin/mvn ${args}"
 }
