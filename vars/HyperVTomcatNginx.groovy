aa616ee1fb8ceace718b6cd2e66be3da8941c789
Zhihui Chen <zhihuic@quarkfinance.com>
2017-10-10 09:27:52 +0000

    Update HyperVTomcatNginx.groovy

--- vars/HyperVTomcatNginx.groovy
+++ vars/HyperVTomcatNginx.groovy
@@ -21,18 +21,18 @@ def call(body) {
 	def saltmasterIP="${config.saltmasterIP}";
 	//upstream 后端主机port
 	def APP_PORT="${config.APP_PORT}";
-	def APP_HOSTIP=config.APP_HOSTS.split(',')
-	def APP_HOSTNAMES=config.APP_HOSTNAMES.split(',')
+	def APP_HOSTIP=config.APP_HOSTS.split(',');
+	def APP_HOSTNAMES=config.APP_HOSTNAMES.split(',');
 	
 	
 	for (i = 0; i <config.APP_HOSTS.size(); i++) {
 		//down nginx upstream host地址
 		def APP_HOST=APP_HOSTIP[i].split(',')[0].trim();
 		def APP_HOSTNAME=APP_HOSTNAMES[i].split(',')[0].trim();
-		println  " ${i} ${APP_HOST} ${APP_HOSTNAME}"
-		println "-------------------------------------ready for ${NgHostName} nginx down ${APP_HOST} ${APP_PORT} "
-		sh (script: "ssh ${saltmasterIP}  'sudo salt -L "${NgHostName}" cmd.script salt://scripts/nginx_up_down.sh 'down ${NGINX_CONF} ${NGINX_DAEMON} ${APP_HOST} ${APP_PORT}' ' ",returnStdout: true)
-		//sh returnStatus: true, script: "ssh ${saltmasterIP} 'sudo salt -L '${NgHostName}' cmd.script salt://scripts/nginx_up_down.sh args="down ${NGINX_CONF} ${NGINX_DAEMON} ${APP_HOST} ${APP_PORT}"'"
+		println  " ${i} ${APP_HOST} ${APP_HOSTNAME}";
+		println "-------------------------------------ready for ${NgHostName} nginx down ${APP_HOST} ${APP_PORT} ";
+		sh  "ssh ${saltmasterIP}  'sudo salt -L "${NgHostName}" cmd.script salt://scripts/nginx_up_down.sh 'down ${NGINX_CONF} ${NGINX_DAEMON} ${APP_HOST} ${APP_PORT}' ' ";
+		
 		println "-------------------------------------ok for ${NgHostName} nginx down ${APP_HOST} ${APP_PORT} "
 		//update tomcat war and checkUrl
 		//sh returnStatus: true, script: "ssh ${saltmasterIP} 'sudo salt '${NgHostName}' cmd.script salt://scripts/tomcat_update.sh args="${healthCheckFunction} ${DIR_SRC_UPDATE} ${CheckUrl} ${APP_HOST} ${APP_PORT}" runas="${AppRunAs}" '"
