8ac1b5d29bbd32cc8568f732516253e7deb7336c
Zhihui Chen <zhihuic@quarkfinance.com>
2017-11-06 04:25:21 +0000

    Update gradlewPackage.groovy

--- vars/gradlewPackage.groovy
+++ vars/gradlewPackage.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call(args) {
-  args = null==args ? "-b ${env.WORKSPACE}/build.gradle clean build jenkinsMakePkg -x test" : args
-  sh "chmod +x ${env.WORKSPACE}/gradlew"
-  sh "${env.WORKSPACE}/gradlew ${args}"
+  args = null==args ? "-b ${env.WORKSPACE}/src/build.gradle clean build jenkinsMakePkg -x test" : args
+  sh "chmod +x ${env.WORKSPACE}/src/gradlew"
+  sh "${env.WORKSPACE}/src/gradlew ${args}"
 }
